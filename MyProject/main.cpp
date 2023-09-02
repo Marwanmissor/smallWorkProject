3644bf5e6b25aaba22c90703623f8ceb49eab512
Marwanmissor <marwanmissor108@mail.com>
2023-09-03 02:47:29 +0300

    Created some process

--- MyProject/main.cpp
+++ MyProject/main.cpp
@@ -4,6 +4,34 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    //Declare the variable
+    float Helwan1 , Helwan2,hHelwan;
+    float H1V , H2V , HHV ;
+    char ans;
+    cout << "- Helwan 1 : ";
+    cin >> Helwan1;
+    cout << "\n- Helwan 2 : ";
+    cin >> Helwan2;
+    cout << "\n- Hadayeq Helwan : ";
+    cin >> hHelwan;
+    ansCheck:
+    cout << "\n- Visa or Vodafone Cash ? (Y/N) ";
+    cin >> ans;
+    if(ans == 'Y' || ans == 'y'){
+        cout << "\n Helwan 1 visa : ";
+        cin >> H1V;
+        cout << "\n Helwan 2 visa : ";
+        cin >> H2V;
+        cout << "\n Hadayeq Helwan visa : ";
+        cin >> HHV;
+    }
+    else if (ans == 'n' || ans == 'Y'){
+        cout << "\n OK";
+    }
+    else{
+            cout << "\n Unknown Answer Choose Again\n";
+        goto ansCheck;
+    }
+    cout << "\n hi";
     return 0;
 }
